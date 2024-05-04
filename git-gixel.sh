e4de957f85605c4b47fe5a633076057e1d645b19
James Montebell <jamesmontebell4@gmail.com>
2024-05-04 17:31:19 -0400

    maybe

--- git-gixel.sh
+++ git-gixel.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Enter commit message: "
-read message
+# Check if there are any command line arguments
+if [ $# -eq 0 ]; then
+    echo "Please provide a commit message"
+    exit 1
+fi
 
-git commit -m "$message"
+# Read the input provided
+commit_message="$1"
 
-current_directory=$(pwd)
-
-exit
+git commit -m "$commit_message"
+exit 
