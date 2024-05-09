7f81a7d56f5e8cf90e499dd1a3919c3069277556
James Montebell <jamesmontebell4@gmail.com>
2024-05-08 22:06:31 -0400

    start server/database

---
+++ server/script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Character (
+    user_id INTEGER PRIMARY KEY
+    user_email VARCHAR(256) NOT NULL
+    name VARCHAR(64) NOT NULL
+    level INTEGER NOT NULL
+    experience INTEGER NOT NULL
+);
