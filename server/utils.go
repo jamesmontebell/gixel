05899f62a9404bb346bee0f9531e10f405385a20
James Montebell <jamesmontebell4@gmail.com>
2024-05-18 22:03:25 -0400

    calculate experience util function

--- server/utils.go
+++ server/utils.go
@@ -1,20 +1,46 @@
 package server
 
-func calculateLevel(email string) int {
-	level, err := dbConnection.Exec("SELECT level FROM Character WHERE user_email=?", email)
+import "errors"
+
+func calculateLevel(e Experience) (int, int, error) {
+
+	exp, err := getExperience(e.UserEmail)
 	if err != nil {
-		return 0
+		return 0, 0, errors.New("error getExperience")
 	}
 
-	exp, err := dbConnection.Exec("SELECT experience FROM Character WHERE user_email=?", email)
+	level, err := getLevel(e.UserEmail)
 	if err != nil {
-		return 0
+		return 0, 0, errors.New("error getLevel")
 	}
 
+	exp = exp + e.Exp
 	experienceNeeded := (4*level ^ 3) / 5
 
-	if exp >= experienceNeeded {
-
+	for exp >= experienceNeeded {
+		level++
+		exp = exp - experienceNeeded
 	}
 
+	return exp, level, nil
+}
+
+func getExperience(userEmail string) (int, error) {
+	var exp int
+	err := dbConnection.QueryRow("SELECT experience FROM Character WHERE user_email=?", userEmail).Scan(&exp)
+	if err != nil {
+		return 0, errors.New("db error, experience")
+	}
+
+	return exp, nil
+}
+
+func getLevel(userEmail string) (int, error) {
+	var level int
+	err := dbConnection.QueryRow("SELECT level FROM Character WHERE user_email=?", userEmail).Scan(&level)
+	if err != nil {
+		return 0, errors.New("db error, level")
+	}
+
+	return level, nil
 }
